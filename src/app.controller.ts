bef45afeb04690b8947f62a120a192c5956781e0
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-01-15 12:32:39 +0200

    Update app.controller.ts
    added HttpStatus

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHealthCheck() {
     return {
-      status_code: 200,
+      status_code: HttpStatus.OK,
       detail: 'ok',
       result: 'working',
+    };
+  }
+
+  @Get('not-found')
+  getNotFound() {
+    return {
+      status_code: HttpStatus.NOT_FOUND,
+      detail: 'not found',
+      result: 'error',
     };
   }
 }
