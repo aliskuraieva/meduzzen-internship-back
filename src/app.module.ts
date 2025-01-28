637d362d66841dfffb17e40cbbaa4991ec5155ff
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-01-28 13:02:00 +0200

    Fixed import for User entity after folder restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { User } from './entities/user.entity';
+import { User } from './entities/user.entity'; 
 import { Auth } from './entities/auth.entity';
 
 @Module({
